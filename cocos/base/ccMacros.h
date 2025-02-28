8e8e440c43f44066ba2cc2798d460d1497ecfec2
WuJiayiSH <wujiayish@msn.com>
2025-03-01 01:32:33 +0800

    feat: add macros CC_STRINGIFY and CC_TOSTRING

--- cocos/base/ccMacros.h
+++ cocos/base/ccMacros.h
@@ -342,4 +342,7 @@ CC_ASSERT(__gl_error_code == GL_NO_ERROR, "Error"); \
 #define CC_CALLBACK_2(__selector__,__target__, ...) std::bind(&__selector__,__target__, std::placeholders::_1, std::placeholders::_2, ##__VA_ARGS__)
 #define CC_CALLBACK_3(__selector__,__target__, ...) std::bind(&__selector__,__target__, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, ##__VA_ARGS__)
 
+#define CC_STRINGIFY(x) #x
+#define CC_TOSTRING(x) CC_STRINGIFY(x)
+
 #endif // __BASE_CCMACROS_H__
